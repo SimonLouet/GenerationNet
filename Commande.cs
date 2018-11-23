21a5f1e23a45f4e9757a557a76315ce7f686f443
SimonL <simon.louet.98@gmail.com>
2018-11-23 14:37:39 +0100

    Start

---
+++ Commande.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generationnet
+{
+    class Commande
+    {
+
+        private int id;
+        private string date;
+        private string etat;
+        private int client;
+
+        public Commande()
+        {
+        }
+
+        public Commande(int id, string date, string etat, int client)
+        {
+            this.id = id;
+            this.date = date;
+            this.etat = etat;
+            this.client = client;
+            
+        }
+
+
+
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public String getDate()
+        {
+            return date;
+        }
+
+        public void setDate(String date)
+        {
+            this.date = date;
+        }
+
+        public String getEtat()
+        {
+            return etat;
+        }
+
+        public void setEtat(String etat)
+        {
+            this.etat = etat;
+        }
+
+        public int getClient()
+        {
+            return client;
+        }
+
+        public void setClient(int client)
+        {
+            this.client = client;
+        }
+
+
+
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace generationnet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cOMMANDEBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new generationnet.DataSet1();
+            this.cOMMANDETableAdapter = new generationnet.DataSet1TableAdapters.COMMANDETableAdapter();
+            this.dataSet11 = new generationnet.DataSet1();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableAdapterManager = new generationnet.DataSet1TableAdapters.TableAdapterManager();
+            this.cOMMANDEDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.cOMMANDEBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cOMMANDEDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cOMMANDEBindingSource
+            // 
+            this.cOMMANDEBindingSource.DataMember = "COMMANDE";
+            this.cOMMANDEBindingSource.DataSource = this.dataSet1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cOMMANDETableAdapter
+            // 
+            this.cOMMANDETableAdapter.ClearBeforeFill = true;
+            // 
+            // dataSet11
+            // 
+            this.dataSet11.DataSetName = "DataSet1";
+            this.dataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.COMMANDETableAdapter = this.cOMMANDETableAdapter;
+            this.tableAdapterManager.UpdateOrder = generationnet.DataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // cOMMANDEDataGridView
+            // 
+            this.cOMMANDEDataGridView.AutoGenerateColumns = false;
+            this.cOMMANDEDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.cOMMANDEDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.cOMMANDEDataGridView.DataSource = this.cOMMANDEBindingSource;
+            this.cOMMANDEDataGridView.Location = new System.Drawing.Point(33, 107);
+            this.cOMMANDEDataGridView.Name = "cOMMANDEDataGridView";
+            this.cOMMANDEDataGridView.Size = new System.Drawing.Size(393, 354);
+            this.cOMMANDEDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "ID_COMMANDE";
+            this.dataGridViewTextBoxColumn1.HeaderText = "ID_COMMANDE";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "DATE_CREATION";
+            this.dataGridViewTextBoxColumn2.HeaderText = "DATE_CREATION";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "ETAT";
+            this.dataGridViewTextBoxColumn3.HeaderText = "ETAT";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "COM_CLIENT";
+            this.dataGridViewTextBoxColumn4.HeaderText = "COM_CLIENT";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 550);
+            this.Controls.Add(this.cOMMANDEDataGridView);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cOMMANDEBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cOMMANDEDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource cOMMANDEBindingSource;
+        private DataSet1TableAdapters.COMMANDETableAdapter cOMMANDETableAdapter;
+        private DataSet1 dataSet11;
+        private System.Windows.Forms.Label label1;
+        private DataSet1TableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView cOMMANDEDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+    }
+}
+
